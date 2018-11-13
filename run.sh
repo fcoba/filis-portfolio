febfc2d3dea75c9220a1785846ad14e23bf871b3
fcoba <filiscoba1@gmail.com>
2018-11-12 21:38:13 -0500

    Add all project pages and shell script to run (local) projects, to display on site.

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /c/Users/Ryan\ Abrahams/Desktop/Website/filis-web/ImpactHack-migration
+nohup python -m http.server 8001
+
+cd /c/Users/Ryan\ Abrahams/Desktop/Website/filis-web/DATE-a-PARK/parks
+python app.py
